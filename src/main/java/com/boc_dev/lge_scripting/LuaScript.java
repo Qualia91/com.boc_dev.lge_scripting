de57dd92668af08698aac8526082cb070a944563
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2021-01-08 17:42:04 +0000

    added script created example added iml files again because it just makes life easier

--- src/main/java/com/boc_dev/lge_scripting/LuaScript.java
+++ src/main/java/com/boc_dev/lge_scripting/LuaScript.java
@@ -1,7 +1,9 @@
 package com.boc_dev.lge_scripting;
 
+import com.boc_dev.lge_model.gcs.Registry;
 import org.luaj.vm2.Globals;
 import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 import org.luaj.vm2.lib.jse.JsePlatform;
 
 public class LuaScript {
@@ -14,13 +16,10 @@ public class LuaScript {
 
 	}
 
-	public void call(String scriptCode) {
+	public void call(String scriptCode, Registry registry) {
+		globals.set("registry", CoerceJavaToLua.coerce(registry));
 		LuaValue chunk = globals.load(scriptCode);
 		chunk.call();
 	}
 
-	public static void main(String[] args) {
-		LuaScript luaScript = new LuaScript();
-		luaScript.call("print(\"hello, world\")");
-	}
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.boc_dev.lge_scripting {
 	requires luaj.jse.modules;
+	requires com.boc_dev.lge_model;
 	exports com.boc_dev.lge_scripting;
 
 }
